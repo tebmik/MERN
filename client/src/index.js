0196e6e7d0b6b2740aa0f4282d40c40c9df1b3db
tebmik <lloyd.s.tom@gmail.com>
2020-07-20 14:03:42 +0100

    imported ThemeProvider to house the entire App

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import theme from "./utils/theme";
+import GlobalStyles from "./utils/global";
+import { ThemeProvider } from "styled-components";
+
 import App from "./App";
 
 ReactDOM.render(
-    <App />,
+    <ThemeProvider theme={theme}>
+        <>
+            <App />
+            <GlobalStyles />
+        </>
+    </ThemeProvider>,
     document.querySelector("#root")
 );
